1126b8d66b22841d0265d0a6ac6b6b5ef1568a43
mohamed atef <elhawaremohamed7@gmail.com>
2026-07-13 18:06:15 +0300

    feat: json-server, project structure

---
+++ src/app/features/components/page-title-component/page-title-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-title-component',
+  imports: [],
+  templateUrl: './page-title-component.html',
+  styleUrl: './page-title-component.css',
+})
+export class PageTitleComponent {
+
+}
---
+++ src/app/features/components/task-input-component/task-input-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-input-component',
+  imports: [],
+  templateUrl: './task-input-component.html',
+  styleUrl: './task-input-component.css',
+})
+export class TaskInputComponent {
+
+}
