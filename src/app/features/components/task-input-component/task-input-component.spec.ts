3598980d460deae54dd200b496cd0566143436c9
mohamed atef <elhawaremohamed7@gmail.com>
2026-08-21 14:12:42 +0300

    test(task-input-component): implement the structure of the calss test-cases

--- src/app/features/components/task-input-component/task-input-component.spec.ts
+++ src/app/features/components/task-input-component/task-input-component.spec.ts
@@ -1,23 +1,47 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TaskInputComponent } from './task-input-component';
+import { OutputRefSubscription } from '@angular/core';
 
 describe('TaskInputComponent', () => {
-  let component: TaskInputComponent;
   let fixture: ComponentFixture<TaskInputComponent>;
-
+  let taskInputComponent: TaskInputComponent;
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [TaskInputComponent]
-    })
-    .compileComponents();
-
+    TestBed.configureTestingModule({}).compileComponents();
     fixture = TestBed.createComponent(TaskInputComponent);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
+    taskInputComponent = fixture.componentInstance;
+  });
+  it('should create', () => {
+    expect(taskInputComponent).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('when task has meaningful value', () => {
+    let newTask: string = '';
+
+    it('should emit the trimmed value', () => {
+      newTask = ' task 1 ';
+      taskInputComponent.task = newTask;
+      let myEmittedTask: string = '';
+      const subscription: OutputRefSubscription = taskInputComponent.newTask.subscribe(
+        (emittedTask: string) => {
+          myEmittedTask = emittedTask;
+        },
+      );
+      taskInputComponent.onEnter();
+      expect(myEmittedTask).toBe(newTask.trim());
+      subscription.unsubscribe();
+    });
+  });
+  describe('when task is "","  ",null', () => {
+    it('should not emit when task is null', () => {
+      taskInputComponent.task = null;
+      const spyOnEmit = vi.spyOn(taskInputComponent.newTask, 'emit');
+      taskInputComponent.onEnter();
+      expect(spyOnEmit).not.toHaveBeenCalled();
+    });
+    it('should not emit when task is ""', () => {});
+    it('should not emit when task is " "', () => {});
+  });
+  describe('regardless of the task value', () => {
+    it('should set this.task to null', () => {});
   });
 });
